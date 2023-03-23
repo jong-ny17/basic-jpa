66bd76af266f8be5444b85489413b47fe1c33997
jong-ny17 <ffeer0508@gmail.com>
2023-03-23 14:41:53 +0900

    front

--- basic-backend/src/main/java/com/example/basic/controller/TagController.java
+++ basic-backend/src/main/java/com/example/basic/controller/TagController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tags")
 @RequiredArgsConstructor
+@CrossOrigin
 public class TagController {
     private final TagService tagService;
 
---
+++ basic-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello World</div>
+  <button @click="registerUser"> ? </button>
+</template>
+
+<script>
+
+import axios from '@/http.js'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  } , 
+  methods:{
+    registerUser() {
+      axios.get('/api/tags').then
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
